4f99762f2dbbc298c4f356ea3d523110b76e831a
Christopher Ducey <chrisducey01@me.com>
2019-08-19 20:38:10 -0700

    Added win/lose logic and on click events for all 4 buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,83 @@
-var crystal1, crystal2, crystal3, crystal4;
-var numberToReach;
-var userNumber = 0;
+
+var crystalCollector = {
+    crystal1: 0,
+    crystal2: 0,
+    crystal3: 0,
+    crystal4: 0,
+    scoreToReach: 0,
+    userScore: 0,
+    wins: 0,
+    losses: 0,
+
+    randomNumberGenerator : function(start, end){
+        var randomNumber;
+        randomNumber = Math.floor(Math.random() * (end - start) + start);
+        return randomNumber;
+    }
+};
 
 
-function randomNumberGenerator(start, end){
-    var randomNumber;
+function initGame(){
+  crystalCollector.wins = 0;
+  crystalCollector.losses = 0;
 
-    randomNumber = Math.floor(Math.random() * (end - start) + start);
-
-    return randomNumber;
+  initGameTurn();
 }
 
 function initGameTurn(){
-    crystal1 = randomNumberGenerator(1,19);
-    crystal2 = randomNumberGenerator(1,19);
-    crystal3 = randomNumberGenerator(1,19);
-    crystal4 = randomNumberGenerator(1,19);
-    numberToReach = randomNumberGenerator(19,120);
-
-    userNumber = 0;
-
-    $("#numberToReach").text(numberToReach);
+    crystalCollector.crystal1 = crystalCollector.randomNumberGenerator(1,19);
+    crystalCollector.crystal2 = crystalCollector.randomNumberGenerator(1,19);
+    crystalCollector.crystal3 = crystalCollector.randomNumberGenerator(1,19);
+    crystalCollector.crystal4 = crystalCollector.randomNumberGenerator(1,19);
+    crystalCollector.scoreToReach = crystalCollector.randomNumberGenerator(19,120);
+    crystalCollector.userScore = 0;
+    updateUI();
 }
 
-// $(document).ready(){
+function updateUI(){
+    $("#scoreToReach").text(crystalCollector.scoreToReach);
+    $("#userScore").text(crystalCollector.userScore);
+    $("#wins").text(crystalCollector.wins);
+    $("#losses").text(crystalCollector.losses);
+}
 
-// }
+function checkScore(){
+    $("#userScore").text(crystalCollector.userScore);
+
+    if(crystalCollector.userScore > crystalCollector.scoreToReach){
+        alert("YOU LOST THE GAME DUMBASS");
+        crystalCollector.losses++;
+        initGameTurn();
+    }
+    else if(crystalCollector.userScore == crystalCollector.scoreToReach){
+        alert("YOU WON THE GAME KING");
+        crystalCollector.wins++;
+        initGameTurn();
+    }
+}
+
+$(document).ready(function(){
+    initGame();
+});
 
 $("#crystal1").click(function(){
-    userNumber += crystal1;
-    console.log(userNumber);
+    crystalCollector.userScore += crystalCollector.crystal1;
+    checkScore();
 });
 
 $("#crystal2").click(function(){
-    userNumber += crystal2;
-    console.log(userNumber);
+    crystalCollector.userScore += crystalCollector.crystal2;
+    checkScore();
+});
+
+$("#crystal3").click(function(){
+    crystalCollector.userScore += crystalCollector.crystal3;
+    checkScore();
+});
+
+$("#crystal4").click(function(){
+    crystalCollector.userScore += crystalCollector.crystal4;
+    checkScore();
 });
 
 
-initGameTurn();
